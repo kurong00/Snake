da2a984af07458f008a8e8e61ef9f8029e12e926
kurong00 <kurong00@gmail.com>
2017-09-29 15:43:11 +0800

    add move

---
+++ Assets/Scripts/SnakeMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour {
+
+    List<Transform> snakeBodyList = new List<Transform>();
+    float snakeSpeed = 20.0f;
+    float currentSpeed;
+	void Start ()
+    {
+        InitSnakeData();
+    }
+	
+    void InitSnakeData()
+    {
+        currentSpeed = snakeSpeed;
+        snakeBodyList.Insert(0, gameObject.transform);
+    }
+
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        if (snakeBodyList.Count != 0)
+        {
+            snakeBodyList[0].Translate(snakeBodyList[0].forward * currentSpeed * Time.deltaTime);
+        }
+    }
+}
