e975bba15de852adc7f38b2eb674230b3cf0c331
kurong00 <kurong00@gmail.com>
2017-09-29 17:49:05 +0800

    edit move

--- Assets/Scripts/SnakeMove.cs
+++ Assets/Scripts/SnakeMove.cs
@@ -2,31 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeMove : MonoBehaviour {
+public class SnakeMove : MonoBehaviour
+{
 
     List<Transform> snakeBodyList = new List<Transform>();
-    float snakeSpeed = 20.0f;
+    float snakeSpeed = 10.0f;
+    float rotateAngles = 90.0f;
     float currentSpeed;
-	void Start ()
+    Quaternion initRotate;
+    void Start()
     {
         InitSnakeData();
     }
-	
+
     void InitSnakeData()
     {
-        currentSpeed = snakeSpeed;
         snakeBodyList.Insert(0, gameObject.transform);
+        initRotate = snakeBodyList[0].rotation;
     }
 
-	void Update () {
+    void Update()
+    {
         Move();
-	}
+    }
 
     void Move()
     {
+        currentSpeed = snakeSpeed;
+        if (Input.GetKey(KeyCode.UpArrow))
+            currentSpeed *= 2;
         if (snakeBodyList.Count != 0)
         {
-            snakeBodyList[0].Translate(snakeBodyList[0].forward * currentSpeed * Time.deltaTime);
+            snakeBodyList[0].Translate(snakeBodyList[0].forward * currentSpeed * Time.deltaTime,Space.World);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            /*StopAllCoroutines();
+            StartCoroutine(RotateSnakeHead(rotateAngles-180));
+            rotateAngles -= 90;*/
+            snakeBodyList[0].Rotate(Vector3.up * -90 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //StopAllCoroutines();
+            //StartCoroutine(RotateSnakeHead(rotateAngles));
+            //rotateAngles += 90;
+            snakeBodyList[0].Rotate(Vector3.up * 90 * Time.deltaTime);
+        }
+    }
+
+    IEnumerator RotateSnakeHead(float amount)
+    {
+        Quaternion angle = Quaternion.Euler(0, amount, 0) * initRotate;
+        while (snakeBodyList[0].rotation != angle)
+        {
+            snakeBodyList[0].rotation = Quaternion.Lerp(snakeBodyList[0].rotation, angle, Time.deltaTime);
+            yield return null;
         }
     }
 }
